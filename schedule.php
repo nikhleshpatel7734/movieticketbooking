4f20734e2b452048dde2c7685d924f226bfc641c
nikhlesh <146977219+nikhleshpatel7734@users.noreply.github.com>
2023-12-28 14:05:40 +0530

    Add files via upload

---
+++ schedule.php
@@ -0,0 +1,285 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="img/logo.png">
+    <link rel="stylesheet" href="style/styles.css">
+    <title>Movies Schedule</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <script src="_.js "></script>
+</head>
+
+<header></header>
+
+<body>
+    <div class="schedule-section">
+        <h1>Schedule</h1>
+        <div class="schedule-dates">
+            <div class="schedule-item">JANUARY 10,2024</div>
+            <div class="schedule-item ">JANUARY 11,2024</div>
+            <div class="schedule-item">JANUARY 12,2024</div>
+            <div class="schedule-item">JANUARY 13,2024</div>
+            <div class="schedule-item">JANUARY 14,2024</div>
+        </div>
+        <div class="schedule-table">
+            <table>
+                <tr>
+                    <th>SHOW</th>
+                    <th>SCHEDULE IN CINEMA</th>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>JAWAAN</h2>
+                        <i class="far fa-clock"></i> 210m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>Jawan  is a 2023 Indian Hindi-language action thriller film co-written and directed by Atlee in his Hindi film debut.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 PM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 PM</div>
+                                <div class="schedule-item">6:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">1:00 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>WAR</h2>
+                        <i class="far fa-clock"></i> 200m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>Khalid (Tiger Shroff), an Indian Research and Analysis Wing (RAW) agent, is assigned to eliminate Kabir (Hrithik Roshan), a former soldier-turned-rogue, who mentored Khalid
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>AVTAR</h2>
+                        <i class="far fa-clock"></i> 220m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>The Way of Water is a 2022 American epic science fiction film co-produced and directed by James Cameron, who co-wrote the screenplay with Rick Jaffa and Amanda Silver from a story the trio wrote with Josh Friedman and Shane Salerno.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>GADAR 2</h2>
+                        <i class="far fa-clock"></i> 190m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>Gadar 2 was theatrically released on 11 August 2023 and became a commercial success, grossing over ₹691.08 crore (US$87 million) worldwide against a production budget of ₹60 crore (US$7.5 million).
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>KABIR SINGH</h2>
+                        <i class="far fa-clock"></i> 210m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>Kabir, a genius yet hostile medical student, falls in love with Preeti from his college. When Preeti's father spots the couple kissing, he opposes their relationship and decides to marry her off.
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+                <tr class="fade-scroll">
+                    <td>
+                        <h2>PATHAN</h2>
+                        <i class="far fa-clock"></i> 251m <i class="fas fa-desktop"></i> IMAX
+                        <h3>SYNOPSIS</h3>
+                        <p>Pathaan, an exiled RAW agent, works with ISI agent Rubina Mohsin to take down Jim, a former RAW agent, who plans to attack India with a deadly virus. 
+                        </p>
+                        <div class="schedule-item"> DETAILS</a>
+                        </div>
+                    </td>
+                    <td>
+                        <div class="hall-type">
+                            <h3>Private Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>VIP Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                        <div class="hall-type">
+                            <h3>Main Hall</h3>
+                            <div>
+                                <div class="schedule-item"><i class="far fa-clock"></i></div>
+                                <div class="schedule-item">09:00 AM</div>
+                                <div class="schedule-item">11:30 AM</div>
+                                <div class="schedule-item">06:00 PM</div>
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
+
+
+    </div>
+    <footer></footer>
+
+    <script src="scripts/jquery-3.3.1.min.js "></script>
+    <script src="scripts/owl.carousel.min.js "></script>
+    <script src="scripts/script.js "></script>
+</body>
+
+</html>
